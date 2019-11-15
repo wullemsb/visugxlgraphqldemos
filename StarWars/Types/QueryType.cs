b4f5aeb105c003207382dd201802f3b93e31f232
BaWu <wullemsb@gmail.com>
2019-11-15 17:36:28 +0100

    First query

---
+++ StarWars/Types/QueryType.cs
@@ -0,0 +1,21 @@
+﻿using HotChocolate.Types;
+using StarWars.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarWars.Types
+{
+    public class QueryType: ObjectType<Query>
+    {
+        protected override void Configure(IObjectTypeDescriptor<Query> descriptor)
+        {
+            descriptor.Field(t => t.GetHuman(default))
+                .Argument("id", a => a.Type<NonNullType<IdType>>());
+
+            descriptor.Field(t => t.GetDroid(default))
+                .Argument("id", a => a.Type<NonNullType<IdType>>());
+        }
+    }
+}
